ff2f4dc2225b70faa246577c4eff2b9918f511a2
mansicode12 <149291749+mansicode12@users.noreply.github.com>
2025-06-24 13:48:16 +0530

    Time: 1 ms (77.06%) | Memory: 41.8 MB (51.43%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+      String first =   remainInStack(s);
+      String second=   remainInStack(t);
+      return first.equals(second);
+    }
+    private String remainInStack(String str){
+        StringBuilder stack = new StringBuilder();
+        for(char singleChar: str.toCharArray()){
+            if(singleChar != '#'){
+                stack.append(singleChar);
+            }
+            else if(stack.length()>0){
+                stack.deleteCharAt(stack.length()-1);
+            }
+        }
+        return stack.toString();
+    }
+}
